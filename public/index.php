309b7e789a482ccbe1d56c7740280cdd3a4ef1b3
naveena <naveenakurma@gmail.com>
2018-10-05 21:34:45 -0400

    Task 2. created a class and function for reading the csv file and printing the content;

--- public/index.php
+++ public/index.php
@@ -5,3 +5,29 @@
  * Date: 10/5/18
  * Time: 9:18 PM
  */
+
+main::startcsv("student.csv");
+
+class main
+{
+    public static function startcsv($filename)
+    {
+        $records = csv::getRecords($filename);
+        print_r($records);
+    }
+}
+
+class csv
+{
+    public static function getRecords($filename)
+    {
+        $file = fopen($filename, "r");
+        while(!feof($file))
+        {
+            $record = fgetcsv($file);
+            $records[] = $record;
+        }
+        fclose($file);
+        return $records;
+    }
+}
